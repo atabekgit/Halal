8e722f5137bea20ec4c17d178eaca31f77a2a0f0
abdurahimmin <abdurahimtalantbekuulu042@gmail.com>
2022-01-03 14:48:58 +0600

    Xml.abdu.Lishnye_kabinet.menu.Ne_udaetsya_voyti

---
+++ app/src/main/java/com/halal/halal_appmwllc/Lishny/Lishnye_Kabinet_Fragment.kt
@@ -0,0 +1,21 @@
+package com.halal.halal_appmwllc.Lishny
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.halal.halal_appmwllc.R
+
+
+class Lishnye_Kabinet_Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lishnye__kabinet_, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/halal/halal_appmwllc/Lishny/Voyti_Fragment.kt
@@ -0,0 +1,21 @@
+package com.halal.halal_appmwllc.Lishny
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.halal.halal_appmwllc.R
+
+class Voyti_Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_voyti_, container, false)
+    }
+
+}
