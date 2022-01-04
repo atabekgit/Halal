8de0db9c6170cd7d987466d4bdd1b141f43c37f6
abdurahimmin <abdurahimtalantbekuulu042@gmail.com>
2022-01-04 18:09:12 +0600

    new.add.fragments.abdu

---
+++ app/src/main/java/com/halal/halal_appmwllc/questions_i_answers/Questions_i_answersFragment.kt
@@ -0,0 +1,27 @@
+package com.halal.halal_appmwllc.questions_i_answers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.halal.halal_appmwllc.R
+
+
+class Questions_i_answersFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_questions_i_answers, container, false)
+        }
+
+    }
+
