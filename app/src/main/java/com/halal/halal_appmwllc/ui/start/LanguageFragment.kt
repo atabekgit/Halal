65d79b5180f75d5404c43ad4e1a3328e5c6cb066
atabek23 <90192277+atabek23@users.noreply.github.com>
2022-01-11 12:11:51 +0600

    тилди ондоо

--- app/src/main/java/com/halal/halal_appmwllc/ui/start/LanguageFragment.kt
+++ app/src/main/java/com/halal/halal_appmwllc/ui/start/LanguageFragment.kt
@@ -1,5 +1,7 @@
 package com.halal.halal_appmwllc.ui.start
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -15,9 +17,11 @@ import com.halal.halal_appmwllc.ui.MainViewModel
 import com.halal.halal_appmwllc.ui.MainViewModelFactory
 import com.halal.halal_appmwllc.ui.startActivity.ChooseActivity
 import com.halal.repository.Repository
+import kotlinx.android.synthetic.main.fragment_language.*
+import java.util.*
 
 
-class LanguageFragment : Fragment() {
+class LanguageFragment : Fragment(){
     private lateinit var binding: FragmentLanguageBinding
     private lateinit var list: List<Category>
     private lateinit var viewModel: MainViewModel
@@ -36,6 +40,26 @@ class LanguageFragment : Fragment() {
             (activity as ChooseActivity).navController.navigate(R.id.action_languageFragment_to_chooseFragment)
         }
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fun setAppLocale(context: Context, language: String) {
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+            val config = context.resources.configuration
+            config.setLocale(locale)
+            context.createConfigurationContext(config)
+            context.resources.updateConfiguration(config, context.resources.displayMetrics)
+        }
+        radio_groupLang.setOnCheckedChangeListener { radioGroup, i ->
+            when (i){
+                R.id.radioBtnKyr->setAppLocale(requireActivity(), "ky")
+                R.id.radioBtnPus->setAppLocale(requireActivity(), "ru")
+            }
+        }
+
+
     }
 
 }
